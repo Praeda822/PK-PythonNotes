3a0eebfe35bab08af287a65f16809be330df7a29
Patrick Kelly <patdkelly5@gmail.com>
2025-01-06 10:05:07 +1100

    Added test file, basic print statements

---
+++ main.py
@@ -0,0 +1,4 @@
+# print is basically the console.log of python as it prints my string to the console.
+print("This is a simple Python string")
+
+# Since I've installed the Python extension for VSCode, I can run this file by right-clicking and selecting "Run Python File in Terminal" from the context menu.
